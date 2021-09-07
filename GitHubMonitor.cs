aff8f66147ad1e9818ac4e5605432c2ece09c88e
M F M Fazrin <mfmfazrin1986@gmail.com>
2021-09-07 19:46:18 +0530

    Add project files.

---
+++ GitHubMonitor.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace HttpTrigger
+{
+    public static class GitHubMonitor
+    {
+        [FunctionName("GitHubMonitor")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]
+            HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("GitHub Monitor Processed an Action");
+
+            string name = req.Query["name"];
+
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            var responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
